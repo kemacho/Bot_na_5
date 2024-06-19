df8f713ab8d0f9c60e294ce8abd7d214387dec6c
ilyakvvv <154056667+ilyakvvv@users.noreply.github.com>
2024-06-19 20:33:34 +0000

    from aiogram.types import CallbackQuery from sqlalchemy.ext.asyncio import AsyncSession  from db import async_session_maker, User  async def start_callback(callback: CallbackQuery):     # Отправляем подтверждение обработки коллбэк-запроса     await callback.answer()          # Извлекаем роль пользователя из данных коллбэк-запроса     role = callback.data.split("_")[1]      async with async_session_maker() as session:         session: AsyncSession                  # Если пользователь выбрал роль "преподаватель"         if role == "t":             # Создаём нового пользователя в базе данных             new_user = User(id=callback.from_user.id, name=callback.from_user.username)             session.add(new_user)             await session.commit()                          # Отправляем сообщение пользователю о успешной регистрации             await callback.message.answer(f"Вы зарегистрировались как преподаватель")                  # Если пользователь выбрал другую роль         else:             # Отправляем запрос пользователю на ввод ID преподавателя             await callback.message.answer("Введите ID вашего преподавателя:")

--- main.py
+++ main.py
@@ -16,17 +16,20 @@
 # pip install -r requirements.txt —  загрузить пакеты из файла
 
 
-# импорты
-import logging  # библиотека логирования (журналирование)
-import asyncio  # библиотека для асинхронного программирования
-from aiogram import Bot, Dispatcher, types
-from config import API_TOKEN
-from handlers import register_message_handler
-from handlers import commands_for_bot
-from db import async_create_table
+import asyncio
+import logging
 
+from aiogram import Bot, Dispatcher
+
+from yandex import YandexFunctions
+from config import TOKEN
+from db import async_create_table
+from functions import check_updates
+from handlers import commands_for_bot
+from handlers import register_message_handler
 
 # асинхронный вызов функции - конкурентный вызов с ожиданием события для продолжения процесса выполнения
+
 async def main():
     """Настройки перед запуском"""
 
@@ -34,20 +37,24 @@ async def main():
     logging.basicConfig(level=logging.INFO)
 
     # создание экземпляров классов Bot и Dispatcher
-    bot = Bot(token=TOKEN)
     dp = Dispatcher()
+
+    bot = Bot(token=TOKEN)
 
     # функция для вызова хендлеров из пакета handlers
     register_message_handler(dp)
 
     # передача списка команд боту
     await bot.set_my_commands(commands=commands_for_bot)
+
+    # проверка обновлений в яндекс диске
+    asyncio.create_task(check_updates(bot=bot))
+
     await dp.start_polling(bot)
 
 
 # запуск бота через long_polling
 if __name__ == "__main__":
-    # обработка исключений try-except
     try:
         asyncio.run(async_create_table())
         asyncio.run(main())
